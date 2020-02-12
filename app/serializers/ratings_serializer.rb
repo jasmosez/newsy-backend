4ff18a90b905b094ac2049ec4085de879b4e2da8
jasmosez <jas.schaffer@gmail.com>
2020-02-12 10:10:54 -0500

    ratings controller etc

--- app/serializers/ratings_serializer.rb
+++ app/serializers/ratings_serializer.rb
@@ -1,3 +1,3 @@
 class RatingsSerializer < ActiveModel::Serializer
-  attributes :id, :likeability, :integrity
+  attributes :id, :likeability, :integrity, :author_id, :user_id
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
   resources :publications, only: [:index, :show]
+  resources :ratings, only: [:show, :create, :update, :delete]
   resources :authors, only: [:index, :show, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
